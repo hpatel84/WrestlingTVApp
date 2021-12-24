d895a382cf128122b0290e0e3a8a04330f2a7dd3
Amish Patel <10amish@gmail.com>
2021-12-24 17:26:26 -0500

    Get website html

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,20 @@
 package com.example.myapplication
 
+import android.R.attr
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
+import org.jsoup.Jsoup
+import android.graphics.BitmapFactory
+
+import android.R.attr.bitmap
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import java.io.InputStream
+import java.net.URL
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+
 
 /**
  * Loads [MainFragment].
@@ -16,5 +29,39 @@ class MainActivity : FragmentActivity() {
                 .replace(R.id.main_browse_fragment, MainFragment())
                 .commitNow()
         }
+
+        CoroutineScope(IO).launch {
+            getWebsite()
+        }
     }
+}
+
+ fun getWebsite(): String {
+
+    val url = "http://www.allwrestling.live/"
+//Connect to website
+//Connect to website
+    val document: Document = Jsoup.connect(url).get()
+
+//Get the logo source of the website
+
+//Get the logo source of the website
+    val img: Element? = document.select("img").first()
+// Locate the src attribute
+// Locate the src attribute
+    val imgSrc: String? = img?.absUrl("src")
+// Download image from URL
+// Download image from URL
+    val input: InputStream = URL(imgSrc).openStream()
+// Decode Bitmap
+// Decode Bitmap
+    //  bitmap = BitmapFactory.decodeStream(input)
+
+//Get the title of the website
+
+//Get the title of the website
+    val title = document.title()
+    print(title)
+     return title
+
 }
